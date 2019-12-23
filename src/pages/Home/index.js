fb972587f68d1cedc27a6a60d4faa00ec97ebb4c
Sandro Santos <sandro.dev@gmail.com>
2019-12-23 11:40:58 -0300

    cart reducer listen action type ADD_TO_CART

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 import api from '../../services/api';
@@ -7,7 +8,7 @@ import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -25,6 +26,14 @@ export default class Home extends Component {
     });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -35,7 +44,12 @@ export default class Home extends Component {
             <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => {
+                this.handleAddProduct(product);
+              }}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" />
               </div>
@@ -47,3 +61,4 @@ export default class Home extends Component {
     );
   }
 }
+export default connect()(Home);
